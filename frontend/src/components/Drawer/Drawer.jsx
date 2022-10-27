d53506388eff4f0c163175de2dffabaee535687f
Suraj Zinzuwadia <zinzuwas@uwindsor.ca>
2022-10-27 00:43:42 -0400

    Initial Set up home page

---
+++ frontend/src/components/Drawer/Drawer.jsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import AddNewProfile from './AddNewProfile';
+
+const drawerWidth = 240;
+
+export default function StaticDrawer() {
+  const [state, setState] = React.useState();
+  console.log(state);
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Permanent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {['Inventory Management', 'Suggestions', 'Add New Clothes', 'Add new Profile'].map(
+            (text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                  <ListItemText primary={text} onClick={() => setState(text)} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
+        </List>
+      </Drawer>
+      <>
+        {state === 'Add new Profile' ? <AddNewProfile style={{ align: 'center' }} /> : null}
+        {state === 'Suggestions' ? <h1 key={state}>Hello Suggestions</h1> : null}
+        {state === 'Add New Clothes' ? <h1 key={state}>Hello Add New Clothes</h1> : null}
+        <></>
+      </>
+    </Box>
+  );
+}
