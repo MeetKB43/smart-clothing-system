7200dd1077bf44a6bfe899f356220c980c682c7e
MeetKB43 <115726141+MeetKB43@users.noreply.github.com>
2022-11-08 13:56:55 -0500

    take and put cloths APIs fixed index.js

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var cors = require("cors");
 const dotenv = require('dotenv');
+const util = require('util');
 
 // Initialize environment config
 dotenv.config();
@@ -148,6 +149,8 @@ app.post('/login', function (req,res){
          else
             res.status(403).send("Wrong pin");
       }
+      else
+         res.status(403).send("No such device is registered with system")
    })  
 })
 
@@ -177,14 +180,49 @@ app.post('/register_user', function(req,res){
       else{
          sql = "INSERT INTO `user_profile` VALUES (?,?,?,?,?,?,?,?)";
          con.query(sql, data, function (err, result) {
-            console.log(err);
-
             if (err) throw err;
             res.status(200).send("A new user linked with this device");
          });
       }
    });
 })
+
+app.post('/delete_user',function(req,res){
+   if (!validate_session(req)) {
+      res.status(200).send("Invalid Session");
+      return;
+   }
+
+   var uID = req.body['uID'];
+   var sql = "SELECT * from user_profile WHERE uID = ?"
+   con.query(sql, uID, function (err, result) {
+      if (err) throw err
+      if(result.length != 0){
+         var sql = "DELETE from user_profile WHERE uID = ?"
+         con.query(sql, uID, function(err, result){
+            if (err) throw err
+         })
+
+         sql = "DELETE from inventory WHERE uID = ?"
+         con.query(sql, uID, function(err, result){
+            if (err) throw err
+         })
+
+         sql = "DELETE from cloths WHERE uID = ?"
+         con.query(sql, uID, function(err, result){
+            if (err) throw err
+         })
+
+         res.status(200).send("User deleted");
+      }
+      else{
+         res.status(403).send("User not exist");
+      }
+   })
+   
+})
+
+//app.post('/')
 
 app.post('/add_new_cloth', function(req,res){
    if(!validate_session(req)){
@@ -196,18 +234,37 @@ app.post('/add_new_cloth', function(req,res){
    var RFID = req.body['RFID'];
    var uID = req.body['uID'];
    var cType = req.body['cType'];
+   var deviceID = req.body['deviceID']
 
-   sql = "INSERT INTO cloths VALUES (?,?,?)"
-   con.query(sql,[RFID,uID,cType],function(err,result){
+   sql = "SELECT * FROM cloths WHERE RFID = ?"
+   con.query(sql,RFID,function(err,result){
       if (err) {throw err}
-   })
+      if(result.length> 0){
+         res.status(403).send("RFID is already registered with system")
+      }  
+      else{
+         var sql = "SELECT * FROM user_profile WHERE uID = ? AND deviceID = ?"
+         con.query(sql, [uID,deviceID], function(err, result){
+            if (err) throw err;
+            if(result.length != 0){
+               sql = "INSERT INTO cloths VALUES (?,?,?,?)"
+               con.query(sql,[RFID, deviceID, uID, cType],function(err,result){
+                  if (err) {throw err}
+               })
 
-   sql = "INSERT INTO inventory VALUES (?,?,?)"
-   con.query(sql,[RFID,uID,"0"],function(err,result){
-      if (err) {throw err}
-   })
+               sql = "INSERT INTO inventory VALUES (?,?,?,?)"
+               con.query(sql,[RFID, deviceID, uID,"0"],function(err,result){
+                  if (err) {throw err}
+               })
+               res.status(200).send("Collect and stick RFID on cloth");      
+            }else{
+               res.status(403).send("Invalid user");
+            }
+         })
 
-   res.status(200).send("Collect and stick RFID on cloth");
+         
+      }
+   })
 })
 
 
@@ -217,6 +274,7 @@ app.post('/cloth_scanned',function(req,res){
       return
    }
    var RFID = req.body['RFID'];
+   var deviceID = req.body['deviceID'];
    var pkt_type;
    sql = "SELECT * FROM cloths WHERE RFID = ?"
    con.query(sql,RFID,function(err,result){
@@ -229,19 +287,19 @@ app.post('/cloth_scanned',function(req,res){
                sql = "DELETE FROM inventory WHERE RFID = ?"
                con.query(sql, RFID,function(err,result){
                   if(err) {throw err}
-                  c1 = Scanned_Cloth("take_cloth", 2, RFID);
-                  pkt_type = 2;
+                  sql = "INSERT INTO inventory VALUES ?"
+                  c1 = new Scanned_Cloth("take_cloth", 2, RFID);
                   io.to(socket_devices[deviceID]).emit('RFID scanned',c1) //0: new cloth, 1: put cloth, 2: take cloth
                })
             }
             else{
-               c1 = Scanned_Cloth("put_cloth", 1, RFID);
+               c1 = new Scanned_Cloth("put_cloth", 1, RFID);
                io.to(socket_devices[deviceID]).emit('RFID scanned',c1) //0: new cloth, 1: put cloth, 2: take cloth
             }
          })
       }
       else{
-         c1 = Scanned_Cloth("new_Cloth", 0, RFID);
+         c1 = new Scanned_Cloth("new_Cloth", 0, RFID);
          io.to(socket_devices[deviceID]).emit('RFID scanned',c1) //0: new cloth, 1: put cloth, 2: take cloth
       }
       
@@ -250,7 +308,7 @@ app.post('/cloth_scanned',function(req,res){
 
 })
 
-app.post('/add_cloths',function(req,res){
+app.post('/add_cloths',async function(req,res){
    if(!validate_session(req)){
       res.status(401).send('Invalid Session')
       return 
@@ -258,15 +316,46 @@ app.post('/add_cloths',function(req,res){
 
    var data = [];
    var body = req.body;
+   const query = util.promisify(con.query).bind(con);
+   if(Array.isArray(body['RFID'])){
+      for(var i=0;i<body['RFID'].length;i++){
+         var sql = "SELECT * FROM cloths WHERE RFID = ?"
+         var result,result1;
+         try{
+            result = await query(sql, body['RFID'][i])
+         }finally{}
+         
+         var sql = "SELECT * FROM inventory WHERE RFID = ?"
+         try{
+            result1 = await query(sql, body['RFID'][i])
+         }finally{}
 
-   for(var i=0;i<body['RFID'].length;i++){
-      data.push([body['RFID'][i],body['uID'][i],body['laundryState'][i]])
+         if(result.length>0 && result1.length == 0){
+            data.push([body['RFID'][i],result[0]['deviceID'],result[0]['uID'],body['laundryState'][i]])
+         }
+      }
    }
+   else{
+      var sql = "SELECT * FROM cloths WHERE RFID = ?"
+      var result,result1;
+      try{
+         result = await query(sql, body['RFID'])
+      }finally{}
 
-   sql = "INSERT INTO inventory VALUES ?"
-   con.query(sql,[data],function(err,result){
-      if(err) {throw err}
-   })
+      var sql = "SELECT * FROM inventory WHERE RFID = ?"
+      try{
+         result1 = await query(sql, body['RFID'][i])
+      }finally{}
+      if(result.length>0 && result1.length == 0){
+         data.push([body['RFID'],result[0]['deviceID'],result[0]['uID'],body['laundryState']])
+      }
+   }
+   if(data.length > 0){
+      sql = "INSERT INTO inventory VALUES ?"
+      con.query(sql,[data],function(err,result){
+         if(err) {throw err}
+      })
+   }
    res.status(200).send("Cloths added into inventory");
 })
 
@@ -283,9 +372,36 @@ app.post('/display_users', function(req, res){
    })
 })
 
+app.post('/dashboard', function(req,res){
+   /*if(!validate_session(req)){
+      res.status(401).send('Invalid Session')
+      return;
+   }*/
+   var deviceID = req.body['deviceID'];
+   var sql = "SELECT * FROM user_profile WHERE deviceID = ?"
+   const query = util.promisify(con.query).bind(con);
+   query(sql, deviceID,async function (err, result) {
+      if (err) {throw err}
+         for(i = 0;i < result.length;i++){
+            var username = result[i]['username']
+            var uID = result[i]['uID']
+            var sql1 = "SELECT * from inventory WHERE uID = ?";
+            
+            const query = util.promisify(con.query).bind(con);
+            try {
+               var result1 = await query(sql1,uID);
+               console.log("User: " + username + " has "+ result1.length + " washed cloths in his closet");
+            } finally {
+               
+            }
+         }
+      
+      res.status(200).send("Dashboard");
+   })
+   
+})
 
-
-io.on('connection', function(socket){  
+io.on('connection', function(socket){
    socket.on('connected',function(deviceID1){
       deviceID = deviceID1;
       socket_devices[deviceID] = socket.id;
--- backend/s_closet.sql
+++ backend/s_closet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 27, 2022 at 09:30 PM
+-- Generation Time: Nov 07, 2022 at 03:56 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -30,10 +30,20 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `cloths`;
 CREATE TABLE IF NOT EXISTS `cloths` (
   `RFID` bigint(11) NOT NULL,
+  `deviceID` int(11) NOT NULL,
   `uID` int(11) NOT NULL,
-  `cType` enum('0','1','2','3','4','5','6') NOT NULL,
+  `cType` int(11) NOT NULL,
   PRIMARY KEY (`RFID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cloths`
+--
+
+INSERT INTO `cloths` (`RFID`, `deviceID`, `uID`, `cType`) VALUES
+(123456783, 123456, 5857571, 5),
+(123456784, 123456, 5857571, 6),
+(123456785, 123456, 5857571, 9);
 
 -- --------------------------------------------------------
 
@@ -44,10 +54,42 @@ CREATE TABLE IF NOT EXISTS `cloths` (
 DROP TABLE IF EXISTS `inventory`;
 CREATE TABLE IF NOT EXISTS `inventory` (
   `RFID` bigint(20) NOT NULL,
+  `deviceID` int(11) NOT NULL,
   `uID` int(11) NOT NULL,
   `laundryState` enum('0','1') NOT NULL,
   PRIMARY KEY (`RFID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`RFID`, `deviceID`, `uID`, `laundryState`) VALUES
+(123456783, 123456, 5857571, '0'),
+(123456784, 123456, 5857571, '0'),
+(123456785, 123456, 5857571, '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `super_user`
+--
+
+DROP TABLE IF EXISTS `super_user`;
+CREATE TABLE IF NOT EXISTS `super_user` (
+  `deviceID` int(11) NOT NULL,
+  `devicename` varchar(255) NOT NULL,
+  `pin` int(11) NOT NULL,
+  PRIMARY KEY (`deviceID`),
+  UNIQUE KEY `username` (`devicename`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `super_user`
+--
+
+INSERT INTO `super_user` (`deviceID`, `devicename`, `pin`) VALUES
+(123456, 'Mydevice', 123456);
 
 -- --------------------------------------------------------
 
@@ -57,25 +99,24 @@ CREATE TABLE IF NOT EXISTS `inventory` (
 
 DROP TABLE IF EXISTS `user_profile`;
 CREATE TABLE IF NOT EXISTS `user_profile` (
-  `uID` int(11) NOT NULL,
+  `uID` int(11) NOT NULL AUTO_INCREMENT,
+  `deviceID` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
-  `pin` int(11) NOT NULL,
   `gender` varchar(255) NOT NULL,
   `age` int(11) NOT NULL,
   `city` varchar(255) NOT NULL,
   PRIMARY KEY (`uID`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5857572 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_profile`
 --
 
-INSERT INTO `user_profile` (`uID`, `username`, `firstname`, `lastname`, `pin`, `gender`, `age`, `city`) VALUES
-(5857563, 'meet', 'Meet', 'Kevadiya', 123456, 'male', 22, 'Windsor'),
-(3600902, 'arjun', 'Arjun', 'Kheni', 123456, 'male', 22, 'Windsor');
+INSERT INTO `user_profile` (`uID`, `deviceID`, `username`, `firstname`, `lastname`, `gender`, `age`, `city`) VALUES
+(5857571, 123456, 'meet', 'Meet', 'Kevadiya', 'male', 22, 'Windsor');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
